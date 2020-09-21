22a9b27ddc1b1b0ced9a69f6d9a68a322f93a167
Sumit Garg <gargs473@gmail.com>
2020-09-21 17:04:00 +0530

    Added Gesture Detector on enums

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,17 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 
-const activeCardColor = 0xFF1D1E33;
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
+enum Gender {
+  male,
+  female,
+}
+
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +32,12 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(0xFF1D1E33),
+                    onPress: (){
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.mars,
                       label: 'MALE',
@@ -34,7 +46,12 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: ReusableCard(
-                      colour: Color(activeCardColor),
+                    onPress:  (){
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                      colour: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.venus,
                       label: 'FEMALE',
@@ -44,16 +61,16 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Expanded(child: ReusableCard(colour: Color(activeCardColor))),
+          Expanded(child: ReusableCard(colour: activeCardColor)),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: Color(activeCardColor)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: Color(activeCardColor)),
-                )
+                  child: ReusableCard(colour: activeCardColor),
+                ),
               ],
             ),
           ),
